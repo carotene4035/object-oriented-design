3b5dcda6a177a38dca68f02de73e727e4407b16b
carotene512 <karoten512@gmail.com>
2018-03-15 22:35:12 +0900

    p 145

--- main.rb
+++ main.rb
@@ -1,33 +1,33 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :size, :tape_color,
+    :front_shock, :rear_shock, :style
 
   def initialize(args)
-    @size = args[:size]
-    @tape_color = args[:tape_color]
+    @size        = args[:size]
+    @tape_color  = args[:tape_color]
+    # マウンテンバイクに対応するために追加
+    @style       = args[:style]
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
   end
 
-  # すべての自転車は、デフォルト値として
-  # 同じタイヤサイズとチェーンサイズを持つ
   def spares
-    { chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    if style == :road
+      { chain: '10-speed',
+        tire_size: '23', # milimeters
+        tape_color: tape_color }
+    else
+      { chain: '10-speed',
+        tire_size: '2.1', # inches,
+      rear_shock: rear_shock }
+    end
   end
-
-  # 他にもメソッドがたくさん
 end
 
 bike = Bicycle.new(
-size: 'M',
-tape_color: 'red'
-)
+  style: :mountain,
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox')
 
-bike.size # -> 'M'
 bike.spares
-# -> {:tire_size => '23',
-#     :chain => '10-speed',
-#     :tape_color => 'red'}
-
-
-
