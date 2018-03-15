fcac318fba45215e95a1ad8c4032afefa3c2769d
carotene512 <karoten512@gmail.com>
2018-03-16 07:49:58 +0900

    p 164

--- main.rb
+++ main.rb
@@ -4,8 +4,12 @@ class Bicycle
   #
   def initialize(args={})
     @size      = args[:size]
-    @chain     = args[:chain]
-    @tire_size = args[:tire_size]
+    @chain     = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+  end
+
+  def default_chain # 共通の初期値
+    '10-speed'
   end
 end
 
@@ -16,6 +20,10 @@ class RoadBike < Bicycle
   def initialize(args)
     @tape_color  = args[:tape_color]
     super(args) # RoadBikeはsuperを必ずよばなくてはならなくなった
+  end
+
+  def default_tire_size
+    '23' # サブクラスの初期値
   end
 
   def spares
@@ -32,6 +40,10 @@ class MoutainBike < Bicycle
     @front_shock = args[:front_shock]
     @rear_shock  = args[:rear_shock]
     super(args)
+  end
+
+  def default_tire_size
+    '2.1' # サブクラスの初期値
   end
 
   def spares
