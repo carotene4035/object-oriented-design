ecb8bfe58cee58181835bf0bc24cdf39e009cb3a
carotene512 <karoten512@gmail.com>
2018-03-16 07:13:59 +0900

    p 158

--- main.rb
+++ main.rb
@@ -1,32 +1,32 @@
 class Bicycle
+  attr_reader :size # RoadBikeから昇格
   # コードはすべてRoadBikeにうつされた
+  #
+  def initialize(args)
+    @size        = args[:size]
+  end
 end
 
 class RoadBike < Bicycle
   # かつてのBicycleクラスからのコードを全て含む
-  attr_reader :size, :tape_color,
-    :front_shock, :rear_shock, :style
+  attr_reader :tape_color,
 
   def initialize(args)
-    @size        = args[:size]
     @tape_color  = args[:tape_color]
-    # マウンテンバイクに対応するために追加
-    @style       = args[:style]
-    @front_shock = args[:front_shock]
-    @rear_shock  = args[:rear_shock]
+    super(args) # RoadBikeはsuperを必ずよばなくてはならなくなった
   end
 
-  def spares
-    if style == :road
-      { chain: '10-speed',
-        tire_size: '23', # milimeters
-        tape_color: tape_color }
-    else
-      { chain: '10-speed',
-        tire_size: '2.1', # inches,
-      rear_shock: rear_shock }
-    end
-  end
+#  def spares
+#    if style == :road
+#      { chain: '10-speed',
+#        tire_size: '23', # milimeters
+#        tape_color: tape_color }
+#    else
+#      { chain: '10-speed',
+#        tire_size: '2.1', # inches,
+#      rear_shock: rear_shock }
+#    end
+#  end
 end
 
 class MoutainBike < Bicycle
@@ -58,4 +58,4 @@ mountain_bike = MoutainBike.new(
 
 mountain_bike.size
 # NoMethodError: undefined method 'size' <- そりゃそうだよ
-mountain_bike.spares
+# mountain_bike.spares
