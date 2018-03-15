8666ba161d1aff3167f4672e98527b3a55d429e8
carotene512 <karoten512@gmail.com>
2018-03-15 23:23:59 +0900

    p 151

--- main.rb
+++ main.rb
@@ -24,10 +24,26 @@ class Bicycle
   end
 end
 
-bike = Bicycle.new(
-  style: :mountain,
+
+class MoutainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+
+end
+
+mountain_bike = MoutainBike.new(
   size: 'S',
   front_shock: 'Manitou',
   rear_shock: 'Fox')
 
-bike.spares
+mountain_bike.size
+mountain_bike.spares
